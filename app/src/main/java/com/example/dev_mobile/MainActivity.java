55bf9064c4b2fe242d1b02843ce026bafe11c22c
Slider95 <60225063+Slider95@users.noreply.github.com>
2021-01-01 13:44:32 +0100

    calories, distance

--- app/src/main/java/com/example/dev_mobile/MainActivity.java
+++ app/src/main/java/com/example/dev_mobile/MainActivity.java
@@ -7,21 +7,25 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
-    public final static String NAME = "com.example.Dev_Mobile.name";
-    public final static String PASSWORD = "com.example.Dev_Mobile.email";
+    public final static String WEIGHT = "com.example.Dev_Mobile.weight";
+    public final static String HEIGHT = "com.example.Dev_Mobile.height";
+    public final static String GENDER = "com.example.Dev_Mobile.gender";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
-        if(pref.contains("name") && pref.contains("password")){
+        if(pref.contains("weight") && pref.contains("height") && pref.contains("gender")){
             Intent intent = new Intent(this, SecondActivity.class);
-            String name = pref.getString("name", null);
-            String password = pref.getString("password",null);
-            intent.putExtra(NAME, name);
-            intent.putExtra(PASSWORD, password);
+            String weight = pref.getString("weight", null);
+            String height = pref.getString("height",null);
+            String gender = pref.getString("gender",null);
+            intent.putExtra(WEIGHT, weight);
+            intent.putExtra(HEIGHT, height);
+            intent.putExtra(GENDER, gender);
             startActivity(intent);
         }
         else{
@@ -33,16 +37,24 @@ public class MainActivity extends AppCompatActivity {
 
     public void submit(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
-        EditText eTname = (EditText) findViewById(R.id.name);
-        EditText eTpassword = (EditText) findViewById(R.id.password);
-        String name = eTname.getText().toString();
-        String password = eTpassword.getText().toString();
-        intent.putExtra(NAME, name);
-        intent.putExtra(PASSWORD, password);
+        EditText eTweight = (EditText) findViewById(R.id.weight);
+        EditText eTheight = (EditText) findViewById(R.id.height);
+        ToggleButton tBgender = findViewById(R.id.gender);
+        String gender = "m";
+        if(tBgender.isChecked()){
+            gender = "f";
+        }
+        String weight = eTweight.getText().toString();
+        String height = eTheight.getText().toString();
+
+        intent.putExtra(WEIGHT, weight);
+        intent.putExtra(HEIGHT, height);
+        intent.putExtra(GENDER, gender);
         SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
         SharedPreferences.Editor editor = pref.edit();
-        editor.putString("name", name);
-        editor.putString("password", password);
+        editor.putString("weight", weight);
+        editor.putString("height", height);
+        editor.putString("gender", height);
         editor.commit();
 
 
--- app/src/main/java/com/example/dev_mobile/SecondActivity.java
+++ app/src/main/java/com/example/dev_mobile/SecondActivity.java
@@ -1,12 +1,16 @@
 package com.example.dev_mobile;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,18 +19,29 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 public class SecondActivity extends AppCompatActivity implements SensorEventListener, CompoundButton.OnCheckedChangeListener {
+    public static final int PERMISSIONS_REQUEST = 0;
     SensorManager sensor;
     TextView steps;
+    TextView kms;
+    TextView kcal;
     CheckBox toggleSteps;
     Sensor stepCounter;
+    String weight;
+    String height;
+    String gender;
+    float currSpeed;
+    Double stepLength;
     boolean running = false;
     public static String TAG = "SecondActivity";
+    private String locationProvider;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,14 +59,49 @@ public class SecondActivity extends AppCompatActivity implements SensorEventList
         }
         toggleSteps = findViewById(R.id.toggleSteps);
         toggleSteps.setOnCheckedChangeListener(this::onCheckedChanged);
-        String name = intent.getStringExtra(MainActivity.NAME);
-
-        TextView TextView = (TextView) findViewById(R.id.name);
+        weight = intent.getStringExtra(MainActivity.WEIGHT);
+        height = intent.getStringExtra(MainActivity.HEIGHT);
+        gender = intent.getStringExtra(MainActivity.GENDER);
+        stepLength = 0.415 * Integer.valueOf(height); //( 0.415 * height for males, 0.413 for females)
+        if (gender.equals("f")) {
+            stepLength = 0.413 * Integer.valueOf(height);
+        }
+        TextView TextView = (TextView) findViewById(R.id.weight);
         steps = findViewById(R.id.steps);
-
-        TextView.setText("Hello " + name);
-
+        kms = findViewById(R.id.kms);
+        kcal = findViewById(R.id.kcal);
         sensor = (SensorManager) getSystemService(SENSOR_SERVICE);
+//////////////////////////////////////////////////////////GPS///////////////////////////////////////////////
+
+
+        // Check permission
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // Permission already granted
+        } else {
+            // Request permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST);
+
+        }
+
+        // Set location provider.
+        locationProvider = LocationManager.GPS_PROVIDER;
+        // Or use LocationManager.NETWORK_PROVIDER
+
+        // Acquire a reference to the system Location Manager
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+
+        // Define a listener that responds to location updates
+        locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                if (location.hasSpeed()) {
+                    currSpeed = location.getSpeed();
+                }
+            }
+        };
     }
 
     public void submit(View view) {
@@ -64,8 +114,13 @@ public class SecondActivity extends AppCompatActivity implements SensorEventList
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        Toast.makeText(this, "pas", Toast.LENGTH_SHORT).show();
         if (running) {
             steps.setText("" + String.valueOf(event.values[0]));
+            Double v = (stepLength * event.values[0])/100000;
+            kms.setText(v + " kms");
+            Double v2 = v * Double.valueOf(weight);
+            kcal.setText(v2 + "Kcal");
         }
     }
 
@@ -99,12 +154,43 @@ public class SecondActivity extends AppCompatActivity implements SensorEventList
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int request, String permissions[], int[] results) {
+        switch (request) {
+            case PERMISSIONS_REQUEST: {
+
+                // If request is cancelled, the result arrays are empty
+                if (results.length > 0 && results[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission was granted, yay! Do something useful
+                } else {
+
+                    // Permission was denied, boo! Disable the
+                    // functionality that depends on this permission
+                    Toast.makeText(this, "Permission denied to access device's GPS", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            case 1:
+                // If request is cancelled, the result arrays are empty
+                if (results.length > 0 && results[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission was granted, yay! Do something useful
+                } else {
+
+                    // Permission was denied, boo! Disable the
+                    // functionality that depends on this permission
+                    Toast.makeText(this, "Permission denied to access step sensor", Toast.LENGTH_SHORT).show();
+                }
+                return;
+        }
+
+    }
+
     public void MapsOnClick(View view) {
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
     }
     @Override
-    public void onCheckedChanged(CompoundButton toggleSteps, //not working ???
+    public void onCheckedChanged(CompoundButton toggleSteps,
                                  boolean isChecked) {
         Log.d(TAG, "Checkbox changed");
         if (isChecked && stepCounter != null) {
